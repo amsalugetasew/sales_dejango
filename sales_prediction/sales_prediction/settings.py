a17abdbf805c6b7251ceaa2575c77ae7b9288b25
getasew <amsalugetasew12@gmail.com>
2025-01-14 14:41:15 +0300

    Fix Vercel deployment

--- sales_prediction/sales_prediction/settings.py
+++ sales_prediction/sales_prediction/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-03l*z7okuf93tmha587sdfhcw4hdwu@+9g1yi59xfiz-a8+ole
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
